eb65e5c3d2acfa4473f8852d23ac6976e3b617b5
Nguyễn Đức Thông <49530311+NDTPINE@users.noreply.github.com>
2020-09-18 18:34:16 +0700

    Make web by asp.net
    Pratic

---
+++ MakeWebByASP.Net/MakeWebByASP.Net/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MakeWebByASP.Net.Controllers
+{
+    public class HelloController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string ChaoMung(string ten, int tuoi = 1)
+        {
+            return HttpUtility.HtmlEncode("Xin chào " + ten + ". Tuổi của bạn là: " + tuoi);
+        }
+    }
+}
