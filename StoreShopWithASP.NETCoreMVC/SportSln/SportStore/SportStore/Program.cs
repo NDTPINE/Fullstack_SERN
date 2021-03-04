ee271b4604cb02938240c68772bdb839dda4bff4
Nguyễn Đức Thông <49530311+NDTPINE@users.noreply.github.com>
2021-03-04 16:01:33 +0700

    fix host for app run the docker

--- StoreShopWithASP.NETCoreMVC/SportSln/SportStore/SportStore/Program.cs
+++ StoreShopWithASP.NETCoreMVC/SportSln/SportStore/SportStore/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,7 +14,18 @@ namespace SportStore
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+           
+            var config = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("hosting.json", true).Build();
+
+            var host = new WebHostBuilder().UseKestrel(opt => opt.AddServerHeader = false)
+                .UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
