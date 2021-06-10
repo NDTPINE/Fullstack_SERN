7f656a9c99e1ea415d7d9170a4b79ef263714110
Nguyễn Đức Thông <49530311+NDTPINE@users.noreply.github.com>
2021-06-11 00:17:49 +0700

    commit all yesterday about CRUD by BLAZOR

--- StoreShopWithASP.NETCoreMVC/Advanced/Models/Person.cs
+++ StoreShopWithASP.NETCoreMVC/Advanced/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,17 @@ namespace Advanced.Models
     public class Person
     {
         public long PersonId { get; set; }
+        [Required(ErrorMessage = "A firstname is required")]
+        [MinLength(3,ErrorMessage ="Firstname must be 3 or more characters")]
         public string Firstname { get; set; }
+        [Required(ErrorMessage = "A Surname is required")]
+        [MinLength(3, ErrorMessage = "Surname must be 3 or more characters")]
         public string Surname { get; set; }
+        [Required]
+        [Range(1,long.MaxValue,ErrorMessage ="A department must be selected")]
         public long DepartmentId { get; set; }
+        [Required]
+        [Range(1, long.MaxValue, ErrorMessage = "A location must be selected")]
         public long LocationId { get; set; }
         public Department Department { get; set; }
         public Location Location { get; set; }
