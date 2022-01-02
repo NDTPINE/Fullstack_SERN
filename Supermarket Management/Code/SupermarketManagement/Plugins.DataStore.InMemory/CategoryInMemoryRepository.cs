e161b93c439b47605b7d6831c49e6c3a7d1d3fa5
NDThong <nguyenducthong1593@gmail.com>
2022-01-02 16:28:05 +0700

    Done with Delete Category Use Case.

--- Supermarket Management/Code/SupermarketManagement/Plugins.DataStore.InMemory/CategoryInMemoryRepository.cs
+++ Supermarket Management/Code/SupermarketManagement/Plugins.DataStore.InMemory/CategoryInMemoryRepository.cs
@@ -28,8 +28,14 @@ namespace Plugins.DataStore.InMemory
         public void AddCategory(Category category)
         {
             if (categories.Any(x => x.Name.Equals(category.Name, StringComparison.OrdinalIgnoreCase))) return;
-            var maxId = categories.Max(x => x.CategoryId);
-            category.CategoryId = maxId + 1;
+            if (categories != null && categories.Count > 0)
+            {
+                var maxId = categories.Max(x => x.CategoryId);
+                category.CategoryId = maxId + 1;
+            } else
+            {
+                category.CategoryId = 1;
+            }
             categories.Add(category);
         }
 
@@ -47,6 +53,10 @@ namespace Plugins.DataStore.InMemory
         {
             var category = categories?.FirstOrDefault(x => x.CategoryId == categoryId);
             return category;
+        }
+        public void DeleteCategory(int categoryId)
+        {
+            categories?.Remove(GetCategoryById(categoryId));
         }
     }
 }
