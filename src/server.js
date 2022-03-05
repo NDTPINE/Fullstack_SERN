c4ce6b2eec4e8a3d91afac9f00be8048ef401222
NDThong <nguyenducthong1593@gmail.com>
2022-03-06 00:17:47 +0700

    test connect data => ok

--- src/server.js
+++ src/server.js
@@ -2,13 +2,16 @@ import express from "express"
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine.js"
 import initWebRoutes from "./route/web.js";
+import {connectDb} from "./config/connectDb.js"
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extened: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
+
+connectDb();
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
