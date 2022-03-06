d1eee4070c9f084d5ca0da0682afea193f38afeb
NDThong <nguyenducthong1593@gmail.com>
2022-03-06 21:40:32 +0700

    create config.js and test connect database

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const dbConfig = {
+  username: "root",
+  password: null,
+  database: "checkdb",
+  host: "127.0.0.1",
+  dialect: "mysql"
+};
+
+export default dbConfig;
+ 
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,19 @@
+import db from "../models/index.js"
+import sequelize from "sequelize"
 
-const getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
+
+const getHomePage = async (req, res) => {
+    try {
+        let data = await db.users.findAll().then(users => {
+            console.log(JSON.stringify(users))
+        });
+        console.log(data);
+    } catch (error) {
+        console.log("ERROR LOAD USERS");
+        console.log(error.stack);
+    }
+    
+    
 }
 const getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
